c8dac216b9556e44fa9684fef61c6b5ba909166b
vinay <vinay@ubuntu.ubuntu-domain>
2018-06-24 14:28:01 -0500

    Initial Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-    
+          <Layout>
+            <p> This is for testing </p>
+          </Layout>
       </div>
     );
   }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const layout = (props) => (
+    <Aux>
+        <div> toolbar, sidedrawer, backdrop </div>
+        <main>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
